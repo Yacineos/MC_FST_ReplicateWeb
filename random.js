c4b0c977d6980cdb5ec99d32af4b643bc3ed1f63
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-04-29 19:16:35 +0100

    the table just needs animation effect+tablenav

--- random.js
+++ random.js
@@ -19,6 +19,47 @@ function random (id,max){
     
 }
 
+function navTable1(){
+    const elem1 =document.querySelector(".schedule-nav__a1").style;
+    const elem2 =document.querySelector(".schedule-nav__a2").style;
+    const elem3 =document.querySelector(".schedule-nav__a3").style;
+
+        document.getElementById("schedule-day1").classList.add("show");
+    document.getElementById("schedule-day2").classList.remove("show");
+    document.getElementById("schedule-day3").classList.remove("show");
+    console.log(document.querySelector(".schedule-nav__a1"));
+    elem1.color="#ffbd00";
+    elem2.color="#fff";
+    elem3.color="#fff";
+}
+function navTable2(){
+    const elem1 =document.querySelector(".schedule-nav__a1").style;
+    const elem2 =document.querySelector(".schedule-nav__a2").style;
+    const elem3 =document.querySelector(".schedule-nav__a3").style;
+
+    document.getElementById("schedule-day2").classList.add("show");
+    document.getElementById("schedule-day1").classList.remove("show");
+    document.getElementById("schedule-day3").classList.remove("show");
+   
+    elem1.color="#fff";
+    elem2.color="#ffbd00";
+    elem3.color="#fff";
+}
+function navTable3(){
+    const elem1 =document.querySelector(".schedule-nav__a1").style;
+    const elem2 =document.querySelector(".schedule-nav__a2").style;
+    const elem3 =document.querySelector(".schedule-nav__a3").style;
+
+    document.getElementById("schedule-day3").classList.add("show");
+    document.getElementById("schedule-day1").classList.remove("show");
+    document.getElementById("schedule-day2").classList.remove("show");
+
+    elem1.color="#fff";
+    elem2.color="#fff";
+    elem3.color="#ffbd00";
+   
+}
+
 random("about-counters__first-h3",20);
 random("about-counters__second-h3",80);
 random("about-counters__third-h3",199);
