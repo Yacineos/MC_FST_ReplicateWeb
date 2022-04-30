919253e281bfc767a8279f4e5a1cf58bef9a71ae
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-04-30 12:51:18 +0100

    modal done , responsivity left

--- random.js
+++ random.js
@@ -109,6 +109,38 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalOpener");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("modal__exit");
+
+var back = document.getElementsByTagName("main")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  back.style.opacity="0.2";
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 random("about-counters__first-h3",20);
 random("about-counters__second-h3",49);
 random("about-counters__third-h3",98);
